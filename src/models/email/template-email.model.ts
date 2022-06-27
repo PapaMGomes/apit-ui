a94b7bd42e78b83b689938461bc9a7892bbc44b6
Pedro Silva <pedro.silva@cbyk.com.br>
2022-06-27 17:18:14 -0300

    save progress

--- src/models/email/template-email.model.ts
+++ src/models/email/template-email.model.ts
@@ -6,33 +6,27 @@ import CURRICULUM_TEMPLATE from './templates/curriculum'
 import COMPANY_TEMPLATE from './templates/company'
 import CONTACT_TEMPLATE from './templates/contact'
 
+type TemplateModelTypes =
+    | CurriculumRegistrationInterface
+    | PartnerCompanyFormInterface
+    | ContactInterface
 export class TemplateEmailModel {
+    private buildTemplate(template: string, model: TemplateModelTypes) {
+        for (const [key, value] of Object.entries(model)) {
+            template = template.replace(`#{${key}}`, value)
+        }
+
+        return template
+    }
+
     curriculum(model: CurriculumRegistrationInterface) {
         delete model.file
-        let template = CURRICULUM_TEMPLATE
-
-        for (const [key, value] of Object.entries(model)) {
-            template = template.replace(`#{${key}}`, value)
-        }
-
-        return template
+        return this.buildTemplate(CURRICULUM_TEMPLATE, model)
     }
     company(model: PartnerCompanyFormInterface) {
-        let template = COMPANY_TEMPLATE
-
-        for (const [key, value] of Object.entries(model)) {
-            template = template.replace(`#{${key}}`, value)
-        }
-
-        return template
+        return this.buildTemplate(COMPANY_TEMPLATE, model)
     }
     contact(model: ContactInterface) {
-        let template = CONTACT_TEMPLATE
-
-        for (const [key, value] of Object.entries(model)) {
-            template = template.replace(`#{${key}}`, value)
-        }
-
-        return template
+        return this.buildTemplate(CONTACT_TEMPLATE, model)
     }
 }
