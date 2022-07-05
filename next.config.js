2bfbea906b5424e8353fc954d52fa81513a4e664
Pedro Silva <pedro.silva@cbyk.com.br>
2022-07-05 09:10:36 -0300

    export config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: true
+}
 
 module.exports = withPlugins([
     [nextConfig],
