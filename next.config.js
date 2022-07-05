027686827067e9995c0d7a74c3952f33cf9c8652
Pedro Silva <pedro.silva@cbyk.com.br>
2022-07-05 11:05:03 -0300

    generate build

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 
+console.log(`[NEXT_CONFIG]: ${process.env.NODE_ENV}`)
 const nextConfig = {
     reactStrictMode: true,
-    trailingSlash: process.env.NODE_ENV === 'development'
+    trailingSlash: process.env.NODE_ENV.trim() == 'development'
 }
 
 module.exports = withPlugins([
