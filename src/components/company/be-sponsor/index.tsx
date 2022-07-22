180c1bed96dd75f5c5329f3980cba212aeeacb39
Pedro Silva <pedro.silva@cbyk.com.br>
2022-07-22 14:43:24 -0300

    add powerpoint file to company

--- src/components/company/be-sponsor/index.tsx
+++ src/components/company/be-sponsor/index.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
-import { Container, Title, Text } from './styles'
+import { SiMicrosoftpowerpoint } from 'react-icons/si'
+import { Container, Title, Text, Button } from './styles'
 
 const BeSponsor: React.FC = () => {
+    const downloadFile = () => {
+        window.open('/files/patrocinador_saiba_mais.pptx', '_blank')
+    }
+
     return (
         <Container>
             <Title>Seja um patrocinador</Title>
             <Text>
                 A sua empresa tem a oportunidade de patrocinar a APIT. Para
                 isso, criamos o programa APIT atividades, relacionando ações
-                institucionais, educacionais e de eventos. Ao se tornar um pareiro ou
-                patrocinador, você apoiará um projeto realmente relacionado a
-                Responsabilidade Social, agregando valor aos jovens atendidos, à
-                sua empresa e à sociedade como um todo.
+                institucionais, educacionais e de eventos. Ao se tornar um
+                pareiro ou patrocinador, você apoiará um projeto realmente
+                relacionado a Responsabilidade Social, agregando valor aos
+                jovens atendidos, à sua empresa e à sociedade como um todo.
             </Text>
 
             <Text>
                 Um dos diferenciais da APIT é o acompanhamento que nós damos aos
-                nossos jovens e adolescentes, para alavancarem no mercado de trabalho.
+                nossos jovens e adolescentes, para alavancarem no mercado de
+                trabalho.
             </Text>
+
+            <Title className="mt-4 mb-2">Entenda mais como funciona:</Title>
+            <Button onClick={downloadFile}>
+                Abrir Powerpoint
+                <SiMicrosoftpowerpoint />
+            </Button>
         </Container>
     )
 }
--- src/components/company/be-sponsor/styles.ts
+++ src/components/company/be-sponsor/styles.ts
@@ -1,6 +1,6 @@
 import tw from 'twin.macro'
 import styled from 'styled-components'
-import { AppContainer } from '@/styles/css/components'
+import { AppButton, AppContainer } from '@/styles/css/components'
 
 export const Container = styled(AppContainer)`
     ${tw`my-6`}
@@ -9,6 +9,22 @@ export const Container = styled(AppContainer)`
 export const Title = styled.p`
     ${tw`text-lg font-bold mb-6`}
     color: ${({ theme }) => theme.colors.orange};
+
+    &:last-of-type {
+        ${tw`mt-6 mb-2`}
+        color: ${({ theme }) => theme.colors.purple};
+    }
 `
 
 export const Text = styled.p``
+
+export const Button = styled(AppButton)`
+    ${tw`px-6 font-bold text-[80%] flex items-center sm:w-[50%]`}
+
+    background: ${({ theme }) => theme.colors.orange};
+    color: #fff;
+
+    > svg {
+        ${tw`ml-2 text-lg`}
+    }
+`
