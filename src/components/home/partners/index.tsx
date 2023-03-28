7323d05d95cc85bc00424c0976a17c4bbfccbaf2
Pedro Silva <pedro.silva-dev@hotmail.com>
2023-03-28 17:13:50 -0300

    feat: partners list

--- src/components/home/partners/index.tsx
+++ src/components/home/partners/index.tsx
@@ -4,32 +4,30 @@ import SathelLogo from '@/assets/images/partners/sathel.png'
 import SystemLogo from '@/assets/images/partners/system.png'
 import NorteneLogo from '@/assets/images/partners/nortene.png'
 import RedeDorLogo from '@/assets/images/partners/rede-dor.png'
-import CsServicosLogo from '@/assets/images/partners/cs-servicos.png'
+import PapaMikeLogo from '@/assets/images/partners/papa-mike.png'
+import AnhangueraLogo from '@/assets/images/partners/anhanguera.png'
 import AlbertSabinLogo from '@/assets/images/partners/albert-sabin.png'
 import VitalBrasilLogo from '@/assets/images/partners/vital-brasil.png'
 import AmaroJulianaLogo from '@/assets/images/partners/amaro-juliana.png'
 import ControleAnaliticoLogo from '@/assets/images/partners/controle-analitico.png'
-import PapaMikeLogo from '@/assets/images/partners/papa-mike.png'
-import UnoparLogo from '@/assets/images/partners/unopar.png'
 
 import { Container, Content, Title, Card, Image, CardTitle } from './styles'
 
 const Partners: React.FC = () => {
     const items = [
+        { image: '', title: 'Danleste' },
         { image: AlbertSabinLogo, title: 'Albert Sabin' },
         { image: AmaroJulianaLogo, title: 'Amaro Julina' },
         { image: ControleAnaliticoLogo, title: 'Controle Analitico' },
-        { image: CsServicosLogo, title: 'CS Serviços' },
-        { image: '', title: 'Danleste' },
+        { image: '', title: 'Pereiras Diesel' },
         { image: GeofixLogo, title: 'Geofix' },
-        { image: NorteneLogo, title: 'Nortene' },
-        { image: '', title: 'Promoleste' },
-        { image: RedeDorLogo, title: 'Red D’or' },
         { image: SathelLogo, title: 'Sathel' },
+        { image: '', title: 'Promoleste' },
         { image: SystemLogo, title: 'System' },
         { image: VitalBrasilLogo, title: 'Vital Brazil' },
+        { image: '', title: 'Dialto Peças' },
         { image: PapaMikeLogo, title: 'Papa Mike' },
-        { image: UnoparLogo, title: 'Unopar' }
+        { image: AnhangueraLogo, title: 'Anhanguera' }
     ]
 
     return (
